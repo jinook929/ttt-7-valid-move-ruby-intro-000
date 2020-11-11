28dd116401234ecac560232da966065f018ae03e
JINOOK JUNG <jinook929@gmail.com>
2020-11-11 00:45:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
 # code your #valid_move? method here
+
 def valid_move?(board, input)
   index = input.to_i - 1
   if (!(position_taken?(board, index)) )
@@ -7,6 +8,7 @@ def valid_move?(board, input)
     return false
   end
 end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
